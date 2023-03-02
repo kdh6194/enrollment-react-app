18715149974555faa5ae188919cfd71ae5f2703a
kdh6194 <kdh6194@naver.com>
2023-03-02 16:13:48 +0900

    React App 클릭 이벤트 테스트

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.js';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-      <h1>Enrollment App</h1>
-  </React.StrictMode>,document.getElementById('root')
+      <h1>Just React</h1>
+  </React.StrictMode>,document.getElementById('app')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+ReactDOM.render(
+    <React.StrictMode>
+       <App />
+    </React.StrictMode>,document.getElementById('root')
+);
