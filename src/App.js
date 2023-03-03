0b84f1e62df8646ee979070b0ae48c2420120abe
kdh6194 <kdh6194@naver.com>
2023-03-03 09:59:15 +0900

    코드 주석 추가

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import EnrollmentForm from './components/EnrollmentForm'
 
 
-
+// EnrollmentForm이라는 폼을 return 하도록
+// 정의된 App 컴퍼넌트
 const App = () => {
 
     return(
@@ -12,4 +13,6 @@ const App = () => {
     )
 }
 
+// 컴퍼넌트나 모듈형태로 작성하는 경우
+// 기본적으로 내보내기할 함수명 지정
 export default App;
--- src/components/EnrollmentForm.js
+++ src/components/EnrollmentForm.js
@@ -3,20 +3,26 @@ import {useState} from "react";
 import '../App.css'
 
 const EnrollmentForm = () => {
+    // 폼에 입력한 이름/성을 기억하기 위해 state형 변수 선언
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
+
+    // state형 변수에 저장된 이름/성을 환영메세지로 출력하기 위해 선언
     const [welcomeMessage, setWelcomeMessage] = useState("");
 
+    // '등록하기' 버튼 클릭시 이름/성을 환영메세지로 만들어
+    // 폼 아래쪽에 나타냄
     const handleSubmit = (e) => {
          setWelcomeMessage(`환영합니다, ${lastName} ${firstName}님 !!`)
             //굳이 return 할 필요가 없다 바로 작성하게끔
-        e.preventDefault(); // submit 기능 중지  -> 요거 없으면 계속 submit 실행
+        e.preventDefault(); // submit 기능 전파 중지  -> 요거 없으면 계속 submit 실행
     };
     return (
         <div>
             <form className='enrolForm' onSubmit={handleSubmit}>
             <h1>대학생 등록 양식</h1>
             <div><label>First Name</label>
+                {/* onBlur 이벤트 발생시 입력한 이름/성은 firstName, lastName 변수에 저장*/}
                 <input type="text" name="fname"
                 onBlur={(e) => setFirstName(e.target.value)} /></div>
                 {/* 대상(e)이 동작(이벤트 발생)을 한다면 setFirstName에 대상의 입력된 값을 넘겨줘
